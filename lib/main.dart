412cdeab3f8e703a4d8ef045f951ccf0ad5b9071
kshitij619 <64234878+kshitij619@users.noreply.github.com>
2025-01-14 23:36:41 +0530

    Delete lib directory

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:alert_prac/my_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
-}
